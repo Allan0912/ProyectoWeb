5f639ae982566d843858c3892a16b1b26ccb681d
Juan David <riosescudero192004@gmail.com>
2023-06-17 16:28:01 -0500

    eliminando carpeta documentos daniel

--- Documents/Daniel/conexion/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexion = new mysqli('localhost','root','','proyecto1');
-
-if ($conexion -> connect_errno){
-	die('Problemas con el servidor'.$conexion->connect_errno);
-}
-
-
-?>
--- Documents/Daniel/indexJs/ordenes_de_servicio.js
+++
@@ -1,3 +0,0 @@
-document.getElementById(orden),addEventListener("click", function(){
-    window,location.href="../html/Registrar_maquina_orden_servicio.html"
-});
