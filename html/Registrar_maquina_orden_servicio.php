bc6c755ddae9064b76659c6b92d477fe0ecf871c
Allan Guzman <allanheller4@gmail.com>
2023-06-26 14:33:17 -0500

    Arregle el error y actualice el nombre del archivo Actualizar maquina O_R

--- html/Registrar_maquina_orden_servicio.php
+++ html/Registrar_maquina_orden_servicio.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="shortcut icon" href="../imagenes/logo.png" type="image/png">
-    <link rel="stylesheet" type="text/css" href="../css/Registrar_maquina_orden_servicio.css">
+    <link rel="stylesheet" type="text/css" href="../css/actualizar_maquina_O_R.css">
   </head>
   <body>
     <div class="container">
