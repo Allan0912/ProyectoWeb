3be66cc10573d6eddfd56b7b5503dc61b6ef91a7
Allan Guzman <allanheller4@gmail.com>
2023-06-28 23:10:24 -0500

    Pude crear la tabla de orden de servicio

--- html/Ordenes_de_servicio.php
+++ html/Ordenes_de_servicio.php
@@ -7,7 +7,7 @@
     <title>Orden de servicio</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="../css/ordenes_de_servicio.css">
+    <link rel="stylesheet" href="../css/Orden_de_servicio copy.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:rgb(44, 44, 105);">
@@ -71,7 +71,7 @@
         </div>
       </nav>
     <!-----------------------------Codigo de la ordenes de servicio-->
-    <div class="container mt-3">
+    <!--<div class="container mt-3">
         <h2 class="text-center" id="titulo-orden">Historial de orden</h2>
         <div class="row  ordenes mx-lg-5 mt-4" id="boton">
             <div class="col-1 col-xs-1 col-sm-1 col-md-1 col-lg-1">
@@ -88,8 +88,27 @@
             </div>     
         </div>
     </div>  
-
-
+    -->
+    <div class="container">
+      <table>
+        <thead>
+          <tr>
+            <th class="text-center">N°</th>
+            <th class="text-center">Orden de servicio</th>
+            <th>Editar</th>
+            <th>Eliminar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>01</td>
+            <td id="orden_servicio">Nombre de la persona de la orden de servicio</td>
+            <td id="editar"><i class='bx bx-edit-alt'  id="icon"></i></td>
+            <td id="eliminar"><i class='bx bx-message-square-x' id="icon"></i></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     <!---------------------------------REDES SOCIALES----------------------------------------------------------------->
       <div class="row">
         <div class="redes">
