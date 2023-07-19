a19b941052197ccbbbd5688056d89099b75413b5
Allan Guzman <allanheller4@gmail.com>
2023-07-18 21:42:59 -0500

    agrege un nuevo archivo llamado catalogo videos admin

--- html/catalogo_videos_admin.php
+++ html/catalogo_videos_admin.php
@@ -1,13 +1,3 @@
-<?php
-  session_start();
-  $varsesion = $_SESSION['usuario'];
-
-  if($varsesion==null || $varsesion=''){
-  
-    header("location:http://localhost/proyecto/html/Inicio_sesion_Admin.php");
-    die();
-  }
-  ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Automaciacion y mantenimiento</title>
-    <link rel="stylesheet" href="../css/catalogo_admin_inicio_de_sesion.css">
+    <link rel="stylesheet" href="../css/catalogo_videos_admin.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="shortcut icon" href="../imagenes/logo.png" type="image/png">
@@ -30,19 +20,18 @@
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav  mb-2 mb-lg-3 ">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle ms-4" href="#" id="contenido" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Repuestos
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="contenido" style="background-color:rgb(44, 44, 105);">
-                      <li><a class="dropdown-item" href="../html/catalogo_videos_admin.html" id="repuestos">Videos</a></li> 
-                      <li><hr class="dropdown-divider bg-light"></li>
-                      <li><a class="dropdown-item" href="#" id="contacto">Contactanos aqui</a></li>
-                    </ul>
-                </li>
-            </ul>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-2 mb-2 mb-lg-3 mx-3 ">
+            <li class="nav-item dropdown ">
+                <a class="nav-link dropdown-toggle " href="#" id="contenido" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Videos
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="contenido" style="background-color:rgb(44, 44, 105);">
+                  <li><a class="dropdown-item" href="../html/Catalogo_inicio_sesion.html" id="contacto">Repuestos</a></li>
+                </ul>
+            </li>
+          </ul>
+          
             <ul class="navbar-nav me-2 mb-2 mb-lg-3 mx-3 ">
               <li class="nav-item dropdown ">
                   <a class="nav-link dropdown-toggle " href="#" id="contenido" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -89,34 +78,37 @@
               </ul>
            </li>
           </ul>
-
-            <form class="d-flex ms-1 mb-2 mx-4">
-              <input class="form-control me-3" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-warning" type="submit"><i class='bx bx-search'></i></button>
-            </form>
-          </div>
+          <form class="d-flex ms-1 mb-2 mx-4">
+            <input class="form-control me-3" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-warning" type="submit"><i class='bx bx-search'></i></button>
+          </form>
         </div>
-      </nav>
+      </div>
+    </nav>
+ 
+      <!----------------------APARTADO DE VISUALIZACION DE REPUESTOS----------------------------------->
 
-
-
-      <!--Se muestran las imagenes o videos al cliente y al administrador-->
-      <div class="row row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 justify-content-evenly">
-        <div class="col-sm-12 col-md-4 col-lg-3 repuesto1 mt-5 mx-2">
+     <div class="row row-cols-sm-2 row-cols-md-5 row-cols-lg-6 row-cols-xl-6 justify-content-evenly" id="contenedor-videos">
+        <div class="col-sm-12 col-md-6 col-lg-5 video1 mt-5 mx-2" id="video-original">
           <figure>
-            <h5 class="mt-3">TITULO</h5>
-            <img class="repuesto mt-3"src="../imagenes/prueba.jpg" height="160" alt="">  
+            <h5 class="mt-3">Titulo del video</h5>
+            <video src="" height="190" width="370" autoplay muted>
+              <source src="Agregar video"  alt="">  
+            </video>
             <figcaption class="figure-caption mt-1">
-              <p class="align-content-end ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, voluptate.</p>
+              <p class="align-content-end ">Este es una pequeña descripcion del video Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur nihil distinctio vel</p>
 							<a class="ver mx-4" href="#">Ver más</a>
 						</figcaption>           
           </figure>
-        </div> 
-      </div>
+        </div>
+        
+        
 
+
+      </div>
       <div class="row">
         <div class="col-12">
-          <nav aria-label="Page">
+          <nav aria-label="Page barra">
             <ul class="pagination justify-content-center mt-4">
               <li class="page-item"><a class="page-link" href="#">Anterior</a></li>
               <li class="page-item"><a class="page-link" href="#">1</a></li>
@@ -130,7 +122,7 @@
      
 
       
-    <!---------------------------------REDES SOCIALES----------------------------------------------------------------->
+      <!---------------------------------REDES SOCIALES----------------------------------------------------------------->
 
     <div class="row">
        <div class="redes">
@@ -144,6 +136,7 @@
       </div>
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="../indexJs/catalogoVideos.js"></script>
 </body>
 </html>
