8c9255db040ff20f8fe4cb3acbf7934e47de2863
Allan Guzman <allanheller4@gmail.com>
2023-07-18 22:10:13 -0500

    error js

--- indexJs/codigo_recuperacion.js
+++ indexJs/codigo_recuperacion.js
@@ -1,5 +1,5 @@
 let boton = document.getElementById('Siguiente')
 
 boton.addEventListener("click", function(){
-    window.location.href="../html/recuperar_contraseña.html"
+    window.location.href="../html/recuperar_contraseña.php"
 });
