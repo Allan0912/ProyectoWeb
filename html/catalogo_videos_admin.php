ccbd1fe9a4088a3d0ffd0200f5364c0f4fce3e0a
Allan Guzman <allanheller4@gmail.com>
2023-07-20 21:21:03 -0500

    Arregle algunos errores de sintaxis y agregue algunos vinculos

--- html/catalogo_videos_admin.php
+++ html/catalogo_videos_admin.php
@@ -123,20 +123,21 @@
 
       
       <!---------------------------------REDES SOCIALES----------------------------------------------------------------->
-
-    <div class="row">
-       <div class="redes">
+    <footer>
+      <div class="row">
+        <div class="redes">
           <hr class="mt-5">
           <ul class="redes">
               <span class="facebook"><a href="#"><i class='bx bxl-facebook'></i></a></span>
               <span class="whatsapp"><a href="#"><i class='bx bxl-whatsapp'></i></a></span>
               <span class="gmail"><a href="#"><i class='bx bxl-gmail'></i></a></span>
-          </ul>
-           
-      </div>
-  </div>
+          </ul>          
+        </div>
+    </div>
+    </footer>
+  
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="../indexJs/catalogoVideos.js"></script>
-</body>
+  </body>
 </html>
--- html/visualizacion_repuesto_cliente.php
+++ html/visualizacion_repuesto_cliente.php
@@ -20,9 +20,6 @@
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-
-
-
 </head>
 
 <body>
@@ -35,9 +32,7 @@
 
       <p class="nombre">Automaticacion <br> y mantenimiento</p>
 
-
-
-
+    
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 
         <span class="navbar-toggler-icon"></span>
